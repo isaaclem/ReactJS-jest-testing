74f5ad85bd41eaa1334b9df8962f295009ab27c0
isaaclem <lem.chiew.choon@gmail.com>
2018-08-12 11:09:49 +0800

    Explore Enzyme render method which using CheerioJS

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-  return <div>Comment List</div>;
+class CommentList extends Component {
+  renderComments() {
+    return this.props.comments.map(com => {
+      return <li key={com}>{com}</li>;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.renderComments()}
+        </ul>
+      </div>
+    );
+  }
 }
+
+function mapStateToProps(state) {
+  return { comments: state.comments };
+}
+export default connect(mapStateToProps)(CommentList);
